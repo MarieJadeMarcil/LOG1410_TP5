bd9da12d22e27678e922870ab6515de7b41da305
Marie-Jade Marcil <marie-jade.marcil@polymtl.ca>
2022-04-01 11:35:47 -0400

    Ingredient

--- Ingredient.cpp
+++ Ingredient.cpp
@@ -2,33 +2,29 @@
 //  Ingredient.cpp
 //  Implementation of the Class Ingredient
 //  Created on:      19-mars-2022 11:23:22
-//  Original author: François
+//  Original author: FranÃ§ois
 ///////////////////////////////////////////////////////////
 
 #include "Ingredient.h"
 
 
-
-
 Ingredient::Ingredient(std::string name, int duration, const AbsProduct& product)
-	: AbsIngredient(name, duration), m_product(product)
+    : AbsIngredient(name, duration), m_product(product)
 {
 }
 
 Ingredient* Ingredient::clone() const
 {
-	// À compléter pour construire un nouvel objet Ingredient en appelant le constructeur de copie
-	return nullptr; // À remplacer
+    return new Ingredient(*this); 
 }
 
 const AbsProduct& Ingredient::getProduct() const
 {
-	return m_product;
+    return m_product;
 }
 
 std::ostream& Ingredient::printToStream(std::ostream& o) const 
 {
-	// À compléter pour imprimer sur un stream une catégorie et ses produits
-	return o;
+    o << m_product.getName() << m_description; 
+    return o;
 }
-
